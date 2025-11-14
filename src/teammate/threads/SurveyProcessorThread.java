912d00898fecde4e7814fa46bd60dc4a17ed1617
hamdhi-muhammad-24 <hamdhimuhammad024@gmail.com>
2025-11-14 22:32:09 +0530

    Write code for thread classes

--- src/teammate/threads/SurveyProcessorThread.java
+++ src/teammate/threads/SurveyProcessorThread.java
@@ -1,4 +1,26 @@
 package teammate.threads;
 
-public class SurveyProcessorThread {
+import teammate.models.Participant;
+import teammate.services.PersonalityClassifier;
+
+import java.util.List;
+
+public class SurveyProcessorThread extends Thread {
+
+    private List<Participant> participants;
+
+    public SurveyProcessorThread(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    @Override
+    public void run() {
+        PersonalityClassifier classifier = new PersonalityClassifier();
+
+        for (Participant p : participants) {
+            classifier.classify(p);
+        }
+
+        System.out.println("Survey processing completed.");
+    }
 }
--- src/teammate/threads/TeamBuilderThread.java
+++ src/teammate/threads/TeamBuilderThread.java
@@ -1,4 +1,31 @@
 package teammate.threads;
 
-public class TeamBuilderThread {
+import teammate.models.Team;
+import teammate.models.Participant;
+import teammate.services.TeamBuilder;
+
+import java.util.List;
+
+public class TeamBuilderThread extends Thread {
+
+    private List<Participant> participants;
+    private int teamSize;
+    private List<Team> result;
+
+    public TeamBuilderThread(List<Participant> participants, int teamSize) {
+        this.participants = participants;
+        this.teamSize = teamSize;
+    }
+
+    public List<Team> getResult() {
+        return result;
+    }
+
+    @Override
+    public void run() {
+        TeamBuilder builder = new TeamBuilder();
+        result = builder.formTeams(participants, teamSize);
+
+        System.out.println("Team formation completed.");
+    }
 }
