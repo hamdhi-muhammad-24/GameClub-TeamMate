1e6f53bc73c457cef6bd1b43212c8927ce793a9c
hamdhi-muhammad-24 <hamdhimuhammad024@gmail.com>
2025-11-14 22:00:33 +0530

    Write code to PersonalityClassifier class

--- src/teammate/services/PersonalityClassifier.java
+++ src/teammate/services/PersonalityClassifier.java
@@ -1,4 +1,14 @@
 package teammate.services;
 
+import teammate.models.Participant;
+
 public class PersonalityClassifier {
+
+    public void classify(Participant p) {
+        int score = p.getTotalPersonalityScore();
+
+        if (score >= 90) p.setPersonalityType("Leader");
+        else if (score >= 70) p.setPersonalityType("Balanced");
+        else p.setPersonalityType("Thinker");
+    }
 }
