6dc7ba5750c7671e649126a9e969de6e9bf2c058
hamdhi-muhammad-24 <hamdhimuhammad024@gmail.com>
2025-11-15 19:26:43 +0530

    Update PersonalityClassifier

--- src/teammate/services/PersonalityClassifier.java
+++ src/teammate/services/PersonalityClassifier.java
@@ -5,10 +5,16 @@ import teammate.models.Participant;
 public class PersonalityClassifier {
 
     public void classify(Participant p) {
-        int score = p.getTotalPersonalityScore();
 
-        if (score >= 90) p.setPersonalityType("Leader");
-        else if (score >= 70) p.setPersonalityType("Balanced");
-        else p.setPersonalityType("Thinker");
+        int score = p.getPersonalityScore();
+
+        if (score >= 90)
+            p.setPersonalityType("Leader");
+
+        else if (score >= 70)
+            p.setPersonalityType("Balanced");
+
+        else
+            p.setPersonalityType("Thinker");
     }
 }
